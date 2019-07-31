791b3eb9d2bea3fe206a87cf08d701de38cf920d
pej <pejzzango0o@gmail.com>
2019-07-31 16:40:44 +0900

    test 연동

---
+++ src/main/java/kr/or/ddit/test2/HelloWorldServlet.java
@@ -0,0 +1,29 @@
+package kr.or.ddit.test2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String today = sdf.format(date);
+		
+		pw.println("<html>");
+		pw.println("	<body>");
+		pw.println("		<h1>hello, today : " + today + "</h1>");
+		pw.println("	</body>");
+		pw.println("</html>");
+	}
+}
